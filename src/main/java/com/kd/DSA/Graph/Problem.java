7aa8b4ed387d5b79fa17e1710772e8e934c92aee
kishan <dubariyakishan1901@gmail.com>
2025-07-31 23:51:25 +0530

    DSA

--- src/main/java/com/kd/DSA/Graph/Problem.java
+++ src/main/java/com/kd/DSA/Graph/Problem.java
@@ -1,5 +1,7 @@
 package com.kd.DSA.Graph;
 
+import java.util.List;
+
 public interface Problem {
     /**
      *  <a href="https://leetcode.com/problems/course-schedule/">course-schedule</a>
@@ -8,4 +10,23 @@ public interface Problem {
      * @return
      */
     boolean canFinish(int numCourses, int[][] prerequisites);
+
+    /**
+     * <a href="https://leetcode.com/problems/evaluate-division/">evaluate-division</a>
+     * @param equations
+     * @param values
+     * @param queries
+     * @return
+     */
+    double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries);
+
+
+    /**
+     * <a href="https://leetcode.com/problems/maximum-subsequence-score/">maximum-subsequence-score</a>
+     * @param nums1
+     * @param nums2
+     * @param k
+     * @return
+     */
+    long maxScore(int[] nums1, int[] nums2, int k) ;
 }
--- src/main/java/com/kd/DSA/Graph/Solution.java
+++ src/main/java/com/kd/DSA/Graph/Solution.java
@@ -1,7 +1,6 @@
 package com.kd.DSA.Graph;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -45,4 +44,123 @@ public class Solution implements Problem {
         curr[index] = true;
         return false;
     }
+
+    @Override
+    public double[] calcEquation(List<List<String>> equations,
+                                 double[] values, List<List<String>> queries) {
+        int n = queries.size();
+        double[] result = new double[n];
+
+        Map<String, List<Pair>> graph = new HashMap<>();
+        for (int i = 0; i < equations.size(); i++) {
+            String s1 = equations.get(i).get(0);
+            String s2 = equations.get(i).get(1);
+            double value = values[i];
+            graph.computeIfAbsent(s1, k1 -> new ArrayList<>());
+            graph.computeIfAbsent(s2, k1 -> new ArrayList<>());
+            graph.get(s1).add(new Pair(s2, value));
+            graph.get(s2).add(new Pair(s2, 1 / value));
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[i] = findDist(queries.get(i).get(0), queries.get(i).get(1), graph);
+        }
+
+        return result;
+    }
+
+    private double findDist(String s1, String s2, Map<String, List<Pair>> graph) {
+        if (Objects.equals(s1, s2)) {
+            return 1;
+        }
+        if (!graph.containsKey(s1) || !graph.containsKey(s2)) {
+            return -1.0;
+        }
+        Queue<Pair<String, Long>> queue = new LinkedList<>();
+        queue.add(new Pair(s1, 1l));
+        Set<String> visited = new HashSet<>();
+        while (!queue.isEmpty()) {
+            Pair<String, Long> pair = queue.poll();
+            for (Pair<String, Long> pair1 : graph.get(pair.key)) {
+                if (Objects.equals(pair1.key, s2)) {
+                    return pair.val * pair1.val;
+                }
+                if (!visited.contains(pair1.key)) {
+                    queue.add(new Pair(pair1.key, pair.val * pair1.val));
+                }
+            }
+            visited.add(pair.key);
+        }
+
+        return -1.0;
+    }
+
+    static class Pair<K, V> {
+        K key;
+        V val;
+
+        Pair(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    @Override
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        List<Pair<Integer,Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(new Pair(nums1[i], nums2[i]));
+        }
+
+        list.sort((a, b) -> b.val - a.val);
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        long sum = 0;
+        long result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num1 = list.get(i).key;
+            int num2 = list.get(i).val;
+
+            minHeap.add(num1);
+            sum += num1;
+
+            if (minHeap.size() > k) {
+                sum -= minHeap.poll();
+            }
+
+            if (minHeap.size() == k) {
+                result = Math.max(result, sum * (long) num2);
+            }
+        }
+
+        return result;
+
+
+//        Set<Integer> set = new HashSet<>();
+//        long sum = 0;
+//        for (int i = 0; i < k; i++) {
+//            set.add(list.get(i).key);
+//            sum += list.get(i).val;
+//        }
+//
+//        List<Pair<Integer, Integer>> list2 = new ArrayList<>();
+//        for (int i = 0; i < n; i++) {
+//            list2.add(new Pair<>(i, nums2[i]));
+//        }
+//        list2.sort((a, b) -> a.val - b.val);
+//
+//        long result = 0;
+//        for (int i = 0; i < n - k; i++) {
+//            if (set.contains(i)) {
+//                result = Math.max(result, sum * nums2[i]);
+//            } else {
+//                result = Math.max(result, (sum + nums1[i] - nums1[list.get(k).key] - 1) * nums2[i]);
+//            }
+//        }
+//        return result;
+    }
+
 }
